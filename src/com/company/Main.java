77cfdf6b0971e30c7ddc8db9f89974177dc4d763
AndyTraficante <andyt.0814@gmail.com>
2020-11-13 21:40:23 -0500

    added path values

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,23 @@
 package com.company;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello!");
+            try{
+                BufferedImage image = ImageIO.read(new File("/Users/AndyTraficante/Desktop/sticke2r.png.png"));
+
+                image.getGraphics().drawLine(1, 1, image.getWidth()-1, image.getHeight()-1);
+                image.getGraphics().drawLine(1, image.getHeight()-1, image.getWidth()-1, 1);
+
+                System.out.println(ImageIO.write(image, "png", new File("/Users/AndyTraficante/Desktop/sticke2r.png.png")));
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
     }
 }
