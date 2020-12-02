5f767b8e8ac020961266b78ea432a8c254110959
AndyTraficante <andyt.0814@gmail.com>
2020-12-02 09:00:36 -0500

    done

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import javafx.beans.property.ReadOnlySetProperty;
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
@@ -10,17 +11,38 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Welcome to the map!" + "\n" + "You are located at Alex's house");
+
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Would you like to randomize the edge weights?");
+
+
+
+        boolean response = false;
+        boolean completed = false;
+
+        while(!completed) {
+            String response1 = input.nextLine();
+            if (response1.equalsIgnoreCase("yes") || (response1.equalsIgnoreCase("true"))) {
+                response = true;
+                completed = true;
+            } else if (response1.equalsIgnoreCase("no") || response1.equalsIgnoreCase(("false"))) {
+                completed = true;
+            } else {
+                System.out.println("That is not a valid response");
+            }
+        }
+
+        System.out.println("Welcome to the map!" + "\n" + "You are located at West Palm Node A");
         System.out.println();
-
-
         map map1 = new map();
-        map1.makeGraph();
+        map1.makeGraph(response);
 
 
 
--- src/main/java/com/company/map.java
+++ src/main/java/com/company/map.java
@@ -1,9 +1,13 @@
 package com.company;
 
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import com.mxgraph.layout.mxCircleLayout;
+import com.mxgraph.layout.mxIGraphLayout;
+import com.mxgraph.util.mxCellRenderer;
+//import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.jgrapht.Graph;
 import org.jgrapht.alg.interfaces.ShortestPathAlgorithm;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
+import org.jgrapht.ext.JGraphXAdapter;
 import org.jgrapht.graph.*;
 
 import javax.imageio.ImageIO;
@@ -14,27 +18,39 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class map {
-    int AB = 1;
-    int AC = 1;
-    int CD = 1;
-    int BE = 2;
-    int EJ = 6;
-    int DG = 5;
-    int GH = 7;
-    int HI = 2;
-    int JK = 1;
-    int KL = 2;
-    int LM = 1;
-    int FL = 3;
-    int BF = 6;
-    int IN = 2;
-    int NO = 2;
-    int MO = 1;
-    int OP = 1;
+
+    public int speedLimit;
+
+    public int getSpeedLimit() {
+        return speedLimit;
+    }
+
+    public void setSpeedLimit(int speedLimit) {
+        this.speedLimit = speedLimit;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    private int distance;
+
+    public map(int distance, int speedLimit){
+        this.distance = distance;
+        this.speedLimit = speedLimit;
+    }
+
+    public map() {
+
+    }
 
     String location = "A";
 
-    public void makeGraph() {
+    public void makeGraph(boolean randomize) {
 
         //DefaultDirectedGraph<String, DefaultEdge> g2 = new DefaultDirectedGraph<>(DefaultEdge.class);
 
@@ -57,83 +73,168 @@ public class map {
         g2.addVertex("O");
         g2.addVertex("P");
 
-        DefaultWeightedEdge AB = g2.addEdge("A", "B");
-        g2.setEdgeWeight(AB, 1);
+        if(!randomize) {
+            DefaultWeightedEdge AB = g2.addEdge("A", "B");
+            g2.setEdgeWeight(AB, 1);
 
-        DefaultWeightedEdge AC = g2.addEdge("A", "C");
-        g2.setEdgeWeight(AC, 1);
+            DefaultWeightedEdge AC = g2.addEdge("A", "C");
+            g2.setEdgeWeight(AC, 1);
 
-        DefaultWeightedEdge CD = g2.addEdge("C", "D");
-        g2.setEdgeWeight(CD, 1);
+            DefaultWeightedEdge CD = g2.addEdge("C", "D");
+            g2.setEdgeWeight(CD, 1);
 
-        DefaultWeightedEdge BE = g2.addEdge("B", "E");
-        g2.setEdgeWeight(BE, 2);
+            DefaultWeightedEdge BE = g2.addEdge("B", "E");
+            g2.setEdgeWeight(BE, 2);
 
-        DefaultWeightedEdge EJ = g2.addEdge("E", "J");
-        g2.setEdgeWeight(EJ, 6);
+            DefaultWeightedEdge EJ = g2.addEdge("E", "J");
+            g2.setEdgeWeight(EJ, 6);
 
-        DefaultWeightedEdge DG = g2.addEdge("D", "G");
-        g2.setEdgeWeight(DG, 5);
+            DefaultWeightedEdge DG = g2.addEdge("D", "G");
+            g2.setEdgeWeight(DG, 5);
 
-        DefaultWeightedEdge GH = g2.addEdge("G", "H");
-        g2.setEdgeWeight(GH, 7);
+            DefaultWeightedEdge GH = g2.addEdge("G", "H");
+            g2.setEdgeWeight(GH, 7);
 
-        DefaultWeightedEdge HI = g2.addEdge("H", "I");
-        g2.setEdgeWeight(HI, 2);
+            DefaultWeightedEdge HI = g2.addEdge("H", "I");
+            g2.setEdgeWeight(HI, 2);
 
-        DefaultWeightedEdge JK = g2.addEdge("J", "K");
-        g2.setEdgeWeight(JK, 1);
+            DefaultWeightedEdge JK = g2.addEdge("J", "K");
+            g2.setEdgeWeight(JK, 1);
 
-        DefaultWeightedEdge KL = g2.addEdge("K", "L");
-        g2.setEdgeWeight(KL, 2);
+            DefaultWeightedEdge KL = g2.addEdge("K", "L");
+            g2.setEdgeWeight(KL, 2);
 
-        DefaultWeightedEdge LM = g2.addEdge("L", "M");
-        g2.setEdgeWeight(LM, 1);
+            DefaultWeightedEdge LM = g2.addEdge("L", "M");
+            g2.setEdgeWeight(LM, 1);
 
-        DefaultWeightedEdge FL = g2.addEdge("F", "L");
-        g2.setEdgeWeight(FL, 3);
+            DefaultWeightedEdge FL = g2.addEdge("F", "L");
+            g2.setEdgeWeight(FL, 3);
 
-        DefaultWeightedEdge BF = g2.addEdge("B", "F");
-        g2.setEdgeWeight(BF, 6);
+            DefaultWeightedEdge BF = g2.addEdge("B", "F");
+            g2.setEdgeWeight(BF, 6);
 
-        DefaultWeightedEdge IN = g2.addEdge("I", "N");
-        g2.setEdgeWeight(IN, 2);
+            DefaultWeightedEdge IN = g2.addEdge("I", "N");
+            g2.setEdgeWeight(IN, 2);
 
-        DefaultWeightedEdge NO = g2.addEdge("N", "O");
-        g2.setEdgeWeight(NO, 2);
+            DefaultWeightedEdge NO = g2.addEdge("N", "O");
+            g2.setEdgeWeight(NO, 2);
 
-        DefaultWeightedEdge MO = g2.addEdge("M", "O");
-        g2.setEdgeWeight(MO, 1);
+            DefaultWeightedEdge MO = g2.addEdge("M", "O");
+            g2.setEdgeWeight(MO, 1);
 
-        DefaultWeightedEdge OP = g2.addEdge("O", "P");
-        g2.setEdgeWeight(OP, 1);
+            DefaultWeightedEdge OP = g2.addEdge("O", "P");
+            g2.setEdgeWeight(OP, 1);
 
-        DefaultWeightedEdge DK = g2.addEdge("D", "K");
-        g2.setEdgeWeight(DK, 5);
+            DefaultWeightedEdge DK = g2.addEdge("D", "K");
+            g2.setEdgeWeight(DK, 5);
 
-        DefaultWeightedEdge IL = g2.addEdge("I", "L");
-        g2.setEdgeWeight(IL, 2);
+            DefaultWeightedEdge IL = g2.addEdge("I", "L");
+            g2.setEdgeWeight(IL, 2);
 
-        DefaultWeightedEdge LI = g2.addEdge("L", "I");
-        g2.setEdgeWeight(LI, 2);
+            DefaultWeightedEdge LI = g2.addEdge("L", "I");
+            g2.setEdgeWeight(LI, 2);
 
-        DefaultWeightedEdge HN = g2.addEdge("H", "N");
-        g2.setEdgeWeight(HN, 2);
+            DefaultWeightedEdge HN = g2.addEdge("H", "N");
+            g2.setEdgeWeight(HN, 2);
 
-        DefaultWeightedEdge NH = g2.addEdge("N", "H");
-        g2.setEdgeWeight(NH, 2);
+            DefaultWeightedEdge NH = g2.addEdge("N", "H");
+            g2.setEdgeWeight(NH, 2);
 
-        DefaultWeightedEdge DE = g2.addEdge("D", "E");
-        g2.setEdgeWeight(DE, 1);
+            DefaultWeightedEdge DE = g2.addEdge("D", "E");
+            g2.setEdgeWeight(DE, 1);
 
-        DefaultWeightedEdge ED = g2.addEdge("E", "D");
-        g2.setEdgeWeight(ED, 1);
+            DefaultWeightedEdge ED = g2.addEdge("E", "D");
+            g2.setEdgeWeight(ED, 1);
 
-        DefaultWeightedEdge KI = g2.addEdge("K", "I");
-        g2.setEdgeWeight(KI, 3);
+            DefaultWeightedEdge KI = g2.addEdge("K", "I");
+            g2.setEdgeWeight(KI, 3);
 
-        DefaultWeightedEdge IH = g2.addEdge("I", "H");
-        g2.setEdgeWeight(IH, 4);
+            DefaultWeightedEdge IH = g2.addEdge("I", "H");
+            g2.setEdgeWeight(IH, 3);
+
+        }else{
+            DefaultWeightedEdge AB = g2.addEdge("A", "B");
+            g2.setEdgeWeight(AB, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge AC = g2.addEdge("A", "C");
+            g2.setEdgeWeight(AC, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge CD = g2.addEdge("C", "D");
+            g2.setEdgeWeight(CD, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge BE = g2.addEdge("B", "E");
+            g2.setEdgeWeight(BE, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge EJ = g2.addEdge("E", "J");
+            g2.setEdgeWeight(EJ, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge DG = g2.addEdge("D", "G");
+            g2.setEdgeWeight(DG, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge GH = g2.addEdge("G", "H");
+            g2.setEdgeWeight(GH, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge HI = g2.addEdge("H", "I");
+            g2.setEdgeWeight(HI, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge JK = g2.addEdge("J", "K");
+            g2.setEdgeWeight(JK, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge KL = g2.addEdge("K", "L");
+            g2.setEdgeWeight(KL, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge LM = g2.addEdge("L", "M");
+            g2.setEdgeWeight(LM, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge FL = g2.addEdge("F", "L");
+            g2.setEdgeWeight(FL, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge BF = g2.addEdge("B", "F");
+            g2.setEdgeWeight(BF, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge IN = g2.addEdge("I", "N");
+            g2.setEdgeWeight(IN, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge NO = g2.addEdge("N", "O");
+            g2.setEdgeWeight(NO, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge MO = g2.addEdge("M", "O");
+            g2.setEdgeWeight(MO, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge OP = g2.addEdge("O", "P");
+            g2.setEdgeWeight(OP, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge DK = g2.addEdge("D", "K");
+            g2.setEdgeWeight(DK, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge IL = g2.addEdge("I", "L");
+            g2.setEdgeWeight(IL, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge LI = g2.addEdge("L", "I");
+            g2.setEdgeWeight(LI, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge HN = g2.addEdge("H", "N");
+            g2.setEdgeWeight(HN, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge NH = g2.addEdge("N", "H");
+            g2.setEdgeWeight(NH, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge DE = g2.addEdge("D", "E");
+            g2.setEdgeWeight(DE, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge ED = g2.addEdge("E", "D");
+            g2.setEdgeWeight(ED, (int) (Math.random() * 10) +1);
+
+            DefaultWeightedEdge KI = g2.addEdge("K", "I");
+            g2.setEdgeWeight(KI, (int)(Math.random()*10)+1);
+
+            DefaultWeightedEdge IH = g2.addEdge("I", "H");
+
+            g2.setEdgeWeight(IH, (int)(Math.random()*10)+1);
+        }
+
+
+
 
 
 
@@ -141,7 +242,7 @@ public class map {
         Scanner userInput = new Scanner(System.in);
         boolean moving = true;
 
-        while(moving) {
+        while (moving) {
             System.out.println("You can move to ");
             int weight = 0;
 
@@ -153,25 +254,46 @@ public class map {
 
 
             }
-            location = userInput.nextLine();
+            location = userInput.nextLine().toUpperCase();
             counter += weight;
             System.out.println("You are now located at " + location + " and have traveled " + counter + " units");
-            if(location.equals("P")){
+            if (location.equals("P")) {
                 moving = false;
             }
         }
+        System.out.println("The shortest path for this map was: ");
         path(g2);
-
-
+        //givenAdaptedGraph_whenWriteBufferedImage_thenFileShouldExist(g2);
 
 
     }
-    public static void path(Graph<String, DefaultWeightedEdge> g2){
+
+    public static void path(Graph<String, DefaultWeightedEdge> g2) {
         DijkstraShortestPath<String, DefaultWeightedEdge> p = new DijkstraShortestPath<String, DefaultWeightedEdge>(g2);
         System.out.println(p.getPath("A", "P"));
-        System.out.println(p.getPathWeight("A", "P"));
+        System.out.println("With a total weight traveled of: ");
+        System.out.println((int)p.getPathWeight("A", "P"));
 
     }
+}
+/*
+    public void givenAdaptedGraph_whenWriteBufferedImage_thenFileShouldExist(Graph g2) throws IOException {
+
+        File imgFile = new File("src/test/resources/graph.png");
+        imgFile.createNewFile();
+        JGraphXAdapter<String, DefaultEdge> graphAdapter = new JGraphXAdapter<String, DefaultEdge>(g2);
+        mxIGraphLayout layout = new mxCircleLayout(graphAdapter);
+        layout.execute(graphAdapter.getDefaultParent());
+
+        BufferedImage image =
+                mxCellRenderer.createBufferedImage(graphAdapter, null, 2, Color.WHITE, true, null);
+        ImageIO.write(image, "PNG", imgFile);
+
+        //assertTrue(imgFile.exists());
     }
+
+ */
+
+
 
 
